cb2a15e9b9a5aebde07cbfd69179f0c55c985773
aidendenden233 <2996876880@qq.com>
2023-07-08 19:46:14 +0800

    更新脚本

--- Assets/AAAAA/Script/TypingSpeedCalculator.cs
+++ Assets/AAAAA/Script/TypingSpeedCalculator.cs
@@ -81,17 +81,16 @@ public class TypingSpeedCalculator : MonoBehaviour
     }
 
     #endregion
-    
+
     private void Start()
     {
         //_dotCilck = GameObject.FindGameObjectWithTag("Dot").GetComponent<DotCilck>();
         inputList.Add(false);
         inputList.Add(false);
         AddListenerEndDelegate(delegate(string message) { LeftHuaDot(); });
-        AddListenerUpdateDelegate(delegate (string message) {scoreCalculation(); });
+        AddListenerUpdateDelegate(delegate(string message) { scoreCalculation(); });
 
         //ContinuousEndDelegate += _dotCilck.HuaDongDot();
-
     }
 
     public void scoreCalculation()
@@ -103,6 +102,7 @@ public class TypingSpeedCalculator : MonoBehaviour
             _dotCilck.DianJiJinDu++;
             Debug.Log(_dotCilck.DianJiJinDu);
         }
+
         Debug.Log(playerFraction);
     }
 
@@ -112,6 +112,7 @@ public class TypingSpeedCalculator : MonoBehaviour
         {
             _dotCilck.HuaDongDotL();
         }
+
         if (HuaDongFangXiang.x > 0)
         {
             _dotCilck.HuaDongDotR();
@@ -120,52 +121,47 @@ public class TypingSpeedCalculator : MonoBehaviour
 
     void Update()
     {
-        if(Input.anyKeyDown) // 检测是否有按键事件发生
+        if (Input.anyKeyDown) // 检测是否有按键事件发生
         {
-            foreach(KeyCode keyCode in System.Enum.GetValues(typeof(MyKeyCode))) // 遍历所有 KeyCode 枚举值
+            foreach (KeyCode keyCode in System.Enum.GetValues(typeof(MyKeyCode))) // 遍历所有 KeyCode 枚举值
             {
-                if(Input.GetKeyDown(keyCode)) // 检测按键是否被按下
+                if (Input.GetKeyDown(keyCode)) // 检测按键是否被按下
                 {
                     inputNow = keyCode;
+                    IsInputFast();
+                    isKeyInputNoInput();
+                    inputACheck();
                     Debug.Log(inputNow); // 添加按下的键到键列表中
                 }
             }
         }
-        IsInputFast();
-        isKeyInputNoInput();
         //Debug.Log(inputList[0]+"|"+ inputList[1]);
-        inputACheck();
     }
 
 
     public bool IsInputFast()
     {
-        if (Input.anyKeyDown)
+        float currentKeyPressTime = Time.time; // 获取当前时间戳
+        float timeSinceLastKeyPress = currentKeyPressTime - lastKeyPressTime; // 计算距离上一次按键事件的时间差
+
+        if (timeSinceLastKeyPress <= timeThreshold)
         {
-            float currentKeyPressTime = Time.time; // 获取当前时间戳
-            float timeSinceLastKeyPress = currentKeyPressTime - lastKeyPressTime; // 计算距离上一次按键事件的时间差
-
-            if (timeSinceLastKeyPress <= timeThreshold)
-            {
-                // Debug.Log("连续按键间隔小于时间阈值：" + timeSinceLastKeyPress + " 秒");
-                lastKeyPressTime = currentKeyPressTime; // 更新上一次按键事件的时间戳
-                inputList.RemoveAt(0);
-                inputList.Add(true);
-                return true;
-            }
-            else
-            {
-                // Debug.Log("按键事件：" + currentKeyPressTime);
-                lastKeyPressTime = currentKeyPressTime; // 更新上一次按键事件的时间戳
-                inputList.RemoveAt(0);
-                inputList.Add(false);
-                return false;
-            }
+            // Debug.Log("连续按键间隔小于时间阈值：" + timeSinceLastKeyPress + " 秒");
+            lastKeyPressTime = currentKeyPressTime; // 更新上一次按键事件的时间戳
+            inputList.RemoveAt(0);
+            inputList.Add(true);
+            return true;
         }
-
+        else
+        {
+            // Debug.Log("按键事件：" + currentKeyPressTime);
+            lastKeyPressTime = currentKeyPressTime; // 更新上一次按键事件的时间戳
+            inputList.RemoveAt(0);
+            inputList.Add(false);
+            return false;
+        }
         //inputList.RemoveAt(0);
         //inputList.Add(false);
-        return false;
     }
 
 
@@ -204,13 +200,13 @@ public class TypingSpeedCalculator : MonoBehaviour
             //StartCoroutine("kaishi");
 
             InputSt = KeyCodeToV(inputNow);
-            
-            nowScreenPosition =GetScreenCoordinates(InputSt);
+
+            nowScreenPosition = GetScreenCoordinates(InputSt);
         }
 
         if (inputList[0] == true && inputList[1] == true)
         {
-            Debug.Log("连续中"); 
+            Debug.Log("连续中");
             UpdateDelegate("chixuzho");
         }
 
@@ -366,154 +362,185 @@ public class TypingSpeedCalculator : MonoBehaviour
             Debug.Log("y为0");
         }
     }
-
-
-
 }
 
- public enum MyKeyCode
-  {
+public enum MyKeyCode
+{
     /// <summary>
     ///   <para>Not assigned (never returned as the result of a keystroke).</para>
     /// </summary>
     None = 0,
-    
+
     Alpha0 = 48, // 0x00000030
+
     /// <summary>
     ///   <para>The '1' key on the top of the alphanumeric keyboard.</para>
     /// </summary>
     Alpha1 = 49, // 0x00000031
+
     /// <summary>
     ///   <para>The '2' key on the top of the alphanumeric keyboard.</para>
     /// </summary>
     Alpha2 = 50, // 0x00000032
+
     /// <summary>
     ///   <para>The '3' key on the top of the alphanumeric keyboard.</para>
     /// </summary>
     Alpha3 = 51, // 0x00000033
+
     /// <summary>
     ///   <para>The '4' key on the top of the alphanumeric keyboard.</para>
     /// </summary>
     Alpha4 = 52, // 0x00000034
+
     /// <summary>
     ///   <para>The '5' key on the top of the alphanumeric keyboard.</para>
     /// </summary>
     Alpha5 = 53, // 0x00000035
+
     /// <summary>
     ///   <para>The '6' key on the top of the alphanumeric keyboard.</para>
     /// </summary>
     Alpha6 = 54, // 0x00000036
+
     /// <summary>
     ///   <para>The '7' key on the top of the alphanumeric keyboard.</para>
     /// </summary>
     Alpha7 = 55, // 0x00000037
+
     /// <summary>
     ///   <para>The '8' key on the top of the alphanumeric keyboard.</para>
     /// </summary>
     Alpha8 = 56, // 0x00000038
+
     /// <summary>
     ///   <para>The '9' key on the top of the alphanumeric keyboard.</para>
     /// </summary>
     Alpha9 = 57, // 0x00000039
     A = 97, // 0x00000061
+
     /// <summary>
     ///   <para>'b' key.</para>
     /// </summary>
     B = 98, // 0x00000062
+
     /// <summary>
     ///   <para>'c' key.</para>
     /// </summary>
     C = 99, // 0x00000063
+
     /// <summary>
     ///   <para>'d' key.</para>
     /// </summary>
     D = 100, // 0x00000064
+
     /// <summary>
     ///   <para>'e' key.</para>
     /// </summary>
     E = 101, // 0x00000065
+
     /// <summary>
     ///   <para>'f' key.</para>
     /// </summary>
     F = 102, // 0x00000066
+
     /// <summary>
     ///   <para>'g' key.</para>
     /// </summary>
     G = 103, // 0x00000067
+
     /// <summary>
     ///   <para>'h' key.</para>
     /// </summary>
     H = 104, // 0x00000068
+
     /// <summary>
     ///   <para>'i' key.</para>
     /// </summary>
     I = 105, // 0x00000069
+
     /// <summary>
     ///   <para>'j' key.</para>
     /// </summary>
     J = 106, // 0x0000006A
+
     /// <summary>
     ///   <para>'k' key.</para>
     /// </summary>
     K = 107, // 0x0000006B
+
     /// <summary>
     ///   <para>'l' key.</para>
     /// </summary>
     L = 108, // 0x0000006C
+
     /// <summary>
     ///   <para>'m' key.</para>
     /// </summary>
     M = 109, // 0x0000006D
+
     /// <summary>
     ///   <para>'n' key.</para>
     /// </summary>
     N = 110, // 0x0000006E
+
     /// <summary>
     ///   <para>'o' key.</para>
     /// </summary>
     O = 111, // 0x0000006F
+
     /// <summary>
     ///   <para>'p' key.</para>
     /// </summary>
     P = 112, // 0x00000070
+
     /// <summary>
     ///   <para>'q' key.</para>
     /// </summary>
     Q = 113, // 0x00000071
+
     /// <summary>
     ///   <para>'r' key.</para>
     /// </summary>
     R = 114, // 0x00000072
+
     /// <summary>
     ///   <para>'s' key.</para>
     /// </summary>
     S = 115, // 0x00000073
+
     /// <summary>
     ///   <para>'t' key.</para>
     /// </summary>
     T = 116, // 0x00000074
+
     /// <summary>
     ///   <para>'u' key.</para>
     /// </summary>
     U = 117, // 0x00000075
+
     /// <summary>
     ///   <para>'v' key.</para>
     /// </summary>
     V = 118, // 0x00000076
+
     /// <summary>
     ///   <para>'w' key.</para>
     /// </summary>
     W = 119, // 0x00000077
+
     /// <summary>
     ///   <para>'x' key.</para>
     /// </summary>
     X = 120, // 0x00000078
+
     /// <summary>
     ///   <para>'y' key.</para>
     /// </summary>
     Y = 121, // 0x00000079
+
     /// <summary>
     ///   <para>'z' key.</para>
     /// </summary>
     Z = 122, // 0x0000007A
-  }
+}
--- Assets/AAAAA/Script/spawn.cs
+++ Assets/AAAAA/Script/spawn.cs
@@ -12,13 +12,14 @@ public class spawn : MonoBehaviour
     {
         if (Input.anyKeyDown)
         {
+            foreach (KeyCode keyCode in System.Enum.GetValues(typeof(MyKeyCode)))
+            {
+                // 在父对象的位置生成一个新的物体
+                GameObject newObject = Instantiate(objectPrefab, _typingSpeedCalculator.GetScreenCoordinates( _typingSpeedCalculator.KeyCodeToV(_typingSpeedCalculator.inputNow)), Quaternion.identity);
+                // 将生成的物体设置为父对象的子对象
 
-            // 在父对象的位置生成一个新的物体
-            GameObject newObject = Instantiate(objectPrefab, _typingSpeedCalculator.GetScreenCoordinates( _typingSpeedCalculator.KeyCodeToV(_typingSpeedCalculator.inputNow)), Quaternion.identity);
-            // 将生成的物体设置为父对象的子对象
-
-            newObject.transform.SetParent(father.transform, false);
-            
+                newObject.transform.SetParent(father.transform, false);
+            }
         }
     }
 }
