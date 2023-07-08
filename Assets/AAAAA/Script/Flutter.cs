d5ef9480993d06e25790bee259257b193bf90ac7
aidendenden233 <2996876880@qq.com>
2023-07-09 04:34:13 +0800

    update 背景飘动

---
+++ Assets/AAAAA/Script/Flutter.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flutter : MonoBehaviour
+{  
+    public List<GameObject> image;
+
+    List<int> count = null;
+    
+    public void FlutterImage(int index)
+    {
+        if (index==1)
+        {
+            pupu(index);
+        }
+
+        if (index==2)
+        {
+            pupu(index);
+        }
+
+        if (index==3)
+        {
+            pupu(index);
+        }
+        
+        if (index==4)
+        {
+            pupu(index);
+        }
+        
+        if (index==5)
+        {
+            pupu(index);
+        }
+        
+        if (index==6)
+        {
+            pupu(index);
+        }
+       
+    }
+
+    public void pupu(int index)
+    {
+        for (int i = 0; i < index; i++)
+        {
+            var n =RandomCount();
+            image[n].SetActive(true);
+            StartCoroutine(SetShow(n));
+        }
+    }
+
+    public int  RandomCount()
+    {
+        if (count.Count>5)
+        {
+            count.Clear();
+        }
+        var num=Random.Range(0, 7);
+        if (!count.Contains(num))
+        {
+            count.Add(num);
+            return num;
+        }
+
+        return RandomCount();
+    }
+    
+    IEnumerator SetShow(int i)
+    {
+        yield return new WaitForSeconds(4.0f);
+        image[i].SetActive(false);
+    }
+}
