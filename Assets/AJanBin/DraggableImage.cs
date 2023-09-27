825f21ffacbfad143dda6a1aac26f5748675f924
aidendenden233 <2996876880@qq.com>
2023-09-27 23:07:18 +0800

    限制移动范围

--- Assets/AJanBin/DraggableImage.cs
+++ Assets/AJanBin/DraggableImage.cs
@@ -6,6 +6,9 @@ public class DraggableImage : MonoBehaviour, IPointerDownHandler, IDragHandler
     private RectTransform imageRectTransform;
     private Canvas canvas;
 
+    [Header("画框移动限制，坐标起始点屏幕中心")]
+    public Vector2 MovementRestrictionsMin;
+    public Vector2 MovementRestrictionsMax;
     private void Start()
     {
         // 获取UI Image的RectTransform组件
@@ -13,6 +16,12 @@ public class DraggableImage : MonoBehaviour, IPointerDownHandler, IDragHandler
 
         // 获取Canvas组件
         canvas = GetComponentInParent<Canvas>();
+        
+        if (MovementRestrictionsMin == Vector2.zero)
+            MovementRestrictionsMin = new Vector2(-Screen.width, -Screen.height);
+        if (MovementRestrictionsMax == Vector2.zero)
+            MovementRestrictionsMax = new Vector2(Screen.width , Screen.height);;
+
     }
 
     public void OnPointerDown(PointerEventData eventData)
@@ -28,6 +37,9 @@ public class DraggableImage : MonoBehaviour, IPointerDownHandler, IDragHandler
         RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, eventData.position, eventData.pressEventCamera, out position);
 
         // 设置UI Image的位置
+        position.x = Mathf.Clamp(position.x, MovementRestrictionsMin.x, MovementRestrictionsMax.x);
+        position.y = Mathf.Clamp(position.y, MovementRestrictionsMin.y, MovementRestrictionsMax.y);
+            
         imageRectTransform.parent.parent.localPosition = position;
     }
 }
