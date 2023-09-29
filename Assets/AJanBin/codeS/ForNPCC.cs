5a38192cf6b9817b7ba89411b8395923867ec9b3
kao_jun <863646260@qq.com>
2023-09-30 03:17:35 +0800

    omg

---
+++ Assets/AJanBin/codeS/ForNPCC.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForNPCC : MonoBehaviour
+{
+
+
+    public GameObject Qustion;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SpawnQustion()
+    {
+        Qustion.SetActive(true);
+    }
+}
