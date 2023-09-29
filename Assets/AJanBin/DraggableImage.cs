f3636334d6033f341928e3e016709886277aa836
aidendenden <2996876880@qq.com>
2023-09-29 22:09:24 +0800

    小改移动

--- Assets/AJanBin/DraggableImage.cs
+++ Assets/AJanBin/DraggableImage.cs
@@ -3,19 +3,23 @@ using UnityEngine.EventSystems;
 
 public class DraggableImage : MonoBehaviour, IPointerDownHandler, IDragHandler
 {
-    private RectTransform imageRectTransform;
-    private Canvas canvas;
-
     [Header("画框移动限制，坐标起始点屏幕中心")]
     public Vector2 MovementRestrictionsMin;
     public Vector2 MovementRestrictionsMax;
+    
+    private Vector2 localMousePos;
+    private Vector3 planeLocalPos;
+    private RectTransform targetObject;
+    private RectTransform parentRectTransform;
+    private RectTransform targetRectTransform;
+    
     private void Start()
     {
         // 获取UI Image的RectTransform组件
-        imageRectTransform = GetComponent<RectTransform>();
-
-        // 获取Canvas组件
-        canvas = GetComponentInParent<Canvas>();
+        targetObject = this.transform.GetComponent<RectTransform>();
+        targetRectTransform = targetObject;
+        parentRectTransform = targetObject.parent as RectTransform;
+        
         
         if (MovementRestrictionsMin == Vector2.zero)
             MovementRestrictionsMin = new Vector2(-Screen.width, -Screen.height);
@@ -27,19 +31,26 @@ public class DraggableImage : MonoBehaviour, IPointerDownHandler, IDragHandler
     public void OnPointerDown(PointerEventData eventData)
     {
         // 将UI Image置于最前方
-        imageRectTransform.SetAsLastSibling();
+        planeLocalPos = targetRectTransform.localPosition;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, eventData.position, eventData.pressEventCamera, out localMousePos);
+        targetObject.gameObject.transform.SetAsLastSibling();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        // 获取鼠标或触摸点的位置
-        Vector2 position;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, eventData.position, eventData.pressEventCamera, out position);
-
-        // 设置UI Image的位置
-        position.x = Mathf.Clamp(position.x, MovementRestrictionsMin.x, MovementRestrictionsMax.x);
-        position.y = Mathf.Clamp(position.y, MovementRestrictionsMin.y, MovementRestrictionsMax.y);
+        Vector2 localPointerPosition;
+        //屏幕点到矩形中的局部点
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, eventData.position, eventData.pressEventCamera, out localPointerPosition))
+        {
+            Vector3 offsetToOriginal = localPointerPosition - localMousePos;
+            Vector3 pos= planeLocalPos + offsetToOriginal;
             
-        imageRectTransform.parent.parent.localPosition = position;
+            pos.x = Mathf.Clamp(pos.x, MovementRestrictionsMin.x, MovementRestrictionsMax.x);
+            pos.y = Mathf.Clamp(pos.y, MovementRestrictionsMin.y, MovementRestrictionsMax.y);
+
+
+            targetObject.parent.parent.localPosition = pos;
+        }
+        
     }
 }
