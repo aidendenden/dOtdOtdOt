e62c81b125d83915696be0dc1a0417020edaf3c9
aidendenden <2996876880@qq.com>
2023-09-29 23:00:03 +0800

    更加优秀的拖拽和显示ip

--- Assets/AJanBin/DraggableImage.cs
+++ Assets/AJanBin/DraggableImage.cs
@@ -3,36 +3,33 @@ using UnityEngine.EventSystems;
 
 public class DraggableImage : MonoBehaviour, IPointerDownHandler, IDragHandler
 {
-    [Header("画框移动限制，坐标起始点屏幕中心")]
-    public Vector2 MovementRestrictionsMin;
-    public Vector2 MovementRestrictionsMax;
-    
     private Vector2 localMousePos;
     private Vector3 planeLocalPos;
     private RectTransform targetObject;
     private RectTransform parentRectTransform;
     private RectTransform targetRectTransform;
-    
-    private void Start()
+
+    [Header("画框移动限制，坐标起始点屏幕中心")] public Vector2 MovementRestrictionsMin;
+    public Vector2 MovementRestrictionsMax;
+
+
+    void Start()
     {
-        // 获取UI Image的RectTransform组件
-        targetObject = this.transform.GetComponent<RectTransform>();
+        targetObject = this.transform.parent.parent.GetComponent<RectTransform>();
         targetRectTransform = targetObject;
         parentRectTransform = targetObject.parent as RectTransform;
-        
-        
+
         if (MovementRestrictionsMin == Vector2.zero)
             MovementRestrictionsMin = new Vector2(-Screen.width, -Screen.height);
         if (MovementRestrictionsMax == Vector2.zero)
-            MovementRestrictionsMax = new Vector2(Screen.width , Screen.height);;
-
+            MovementRestrictionsMax = new Vector2(Screen.width, Screen.height);
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        // 将UI Image置于最前方
         planeLocalPos = targetRectTransform.localPosition;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, eventData.position, eventData.pressEventCamera, out localMousePos);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, eventData.position,
+            eventData.pressEventCamera, out localMousePos);
         targetObject.gameObject.transform.SetAsLastSibling();
     }
 
@@ -40,17 +37,16 @@ public class DraggableImage : MonoBehaviour, IPointerDownHandler, IDragHandler
     {
         Vector2 localPointerPosition;
         //屏幕点到矩形中的局部点
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, eventData.position, eventData.pressEventCamera, out localPointerPosition))
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, eventData.position,
+                eventData.pressEventCamera, out localPointerPosition))
         {
             Vector3 offsetToOriginal = localPointerPosition - localMousePos;
-            Vector3 pos= planeLocalPos + offsetToOriginal;
-            
-            pos.x = Mathf.Clamp(pos.x, MovementRestrictionsMin.x, MovementRestrictionsMax.x);
-            pos.y = Mathf.Clamp(pos.y, MovementRestrictionsMin.y, MovementRestrictionsMax.y);
+            var position = planeLocalPos + offsetToOriginal;
 
+            position.x = Mathf.Clamp(position.x, MovementRestrictionsMin.x, MovementRestrictionsMax.x);
+            position.y = Mathf.Clamp(position.y, MovementRestrictionsMin.y, MovementRestrictionsMax.y);
 
-            targetObject.parent.parent.localPosition = pos;
+            targetObject.localPosition = position;
         }
-        
     }
 }
---
+++ Assets/AJanBin/IPGetter.cs
@@ -0,0 +1,73 @@
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using UnityEngine;
+using Mirror;
+using TMPro;
+using TMPro.Examples;
+using UnityEngine.UI;
+
+using System.Net;
+
+public class IPGetter : MonoBehaviour
+{
+    public TextMeshProUGUI IPText;
+    private void Start()
+    {
+        string ipAddress = GetLocalIPAddress();
+        string ipv4Address = GetLocalIPv4Address();
+        Debug.Log("IP Address: " + ipAddress);
+        Debug.Log("IPv4 Address: " + ipv4Address);
+    }
+
+    public string GetLocalIPAddress()
+    {
+        string ipAddress = string.Empty;
+
+        NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+        foreach (NetworkInterface networkInterface in interfaces)
+        {
+            if (networkInterface.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 || 
+                networkInterface.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+            {
+                foreach (UnicastIPAddressInformation ip in networkInterface.GetIPProperties().UnicastAddresses)
+                {
+                    if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        ipAddress = ip.Address.ToString();
+                        break;
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(ipAddress))
+            {
+                break;
+            }
+        }
+        return ipAddress;
+    }
+    
+    private string GetLocalIPv4Address()
+    {
+        string hostName = Dns.GetHostName();
+        IPHostEntry hostEntry = Dns.GetHostEntry(hostName);
+
+        foreach (IPAddress ip in hostEntry.AddressList)
+        {
+            if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+
+        return string.Empty;
+    }
+    
+
+    public void GetIp()
+    {
+        string ipAddress = GetLocalIPv4Address();
+        IPText.text = ipAddress;
+    }
+}
